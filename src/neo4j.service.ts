efad74c8bc7796bab2775a2d8738adbbf7420d60
Ahmet Berkay Bozkurt <ahmet.bozkurt@signumtte.com>
2022-12-16 09:44:59 +0300

    addSpacesToBlockOrBuilding updated

--- src/neo4j.service.ts
+++ src/neo4j.service.ts
@@ -1696,8 +1696,10 @@ export class Neo4jExcelService implements OnApplicationShutdown {
 
                 let cypher2 = `MATCH (fs:FacilityStructure {realm:"${realm}"})-[:PARENT_OF {isDeleted:false}]-(b:Building {isDeleted:false ,key:"${buildingKey}"})-[:PARENT_OF {isDeleted:false}]->(blck:Block {name:"${data[i][1]}",isDeleted:false})-[:PARENT_OF {isDeleted:false}]->(f:Floor {name:"${data[i][9]}",isDeleted:false}) return f;`
             let checkFloor = await this.read(cypher2);
+            let cypher3 = `MATCH (fs:FacilityStructure {realm:"${realm}"})-[:PARENT_OF {isDeleted:false}]-(b:Building {isDeleted:false ,key:"${buildingKey}"})-[:PARENT_OF {isDeleted:false}]->(blck:Block {name:"${data[i][1]}",isDeleted:false})-[:PARENT_OF* {isDeleted:false}]->(s:Space {name:"${data[i][2]}",isDeleted:false}) return s;`
+            let checkSpace = await this.read(cypher3);
 
-            if(checkFloor.records.length>0) {
+            if(checkFloor.records.length>0 && checkSpace.records.length ==0) {
               let cypher = `MATCH (a:FacilityStructure {realm:"${realm}"})-[:PARENT_OF {isDeleted:false}]->(b:Building {key:"${buildingKey}",isDeleted:false}) \
               MATCH (cont:Contacts {realm:"${realm}"})-[:PARENT_OF {isDeleted:false}]->(p:Contact {email:"${email}",isDeleted:false}) \
               ${createdCypher} \
@@ -1741,7 +1743,7 @@ export class Neo4jExcelService implements OnApplicationShutdown {
                   ["Building"],
                   { key: buildingKey },
                   [`Space`],
-                  { locationCode: data[i][6], isDeleted: false },
+                  { code: data[i][4], isDeleted: false },
                   'PARENT_OF',
                   {isDeleted: false}
                 );
